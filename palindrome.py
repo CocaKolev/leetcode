42cf375bb0856317c764bb368b5a9049d6a19e93
Cocakolev <kiril4444@gmail.com>
2021-11-28 22:19:50 -0600

    testing 1

--- palindrome.py
+++ palindrome.py
@@ -2,7 +2,7 @@ def isPalindrome(x: int) -> bool:
     i:int =0
     reversedX:int=0
     y:int= 0
-
+    
     originalNum=x
     while x!=0:
         modulusX= int(x%10) #find the remainder for the 1s place
