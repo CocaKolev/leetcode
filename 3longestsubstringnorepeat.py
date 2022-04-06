7acd81c5ff7bd4fdb5bbc401c1e99ede1f467f72
CocaKolev <kiril4444@gmail.com>
2022-04-06 00:57:45 -0500

    Completed problem 3

---
+++ 3longestsubstringnorepeat.py
@@ -0,0 +1,25 @@
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longestSubstring = ""
+        currentSubstring = ""
+
+        for firstLetter in range(len(s)):
+            if len(currentSubstring) > len(longestSubstring):
+                longestSubstring = currentSubstring
+                currentSubstring = ""
+            currentSubstring = s[firstLetter]
+            for y in range(firstLetter+1,len(s)):
+                if s[y] not in currentSubstring:
+                    currentSubstring += s[y]
+                else:
+                    break
+
+        return len(longestSubstring)
+
+
+print(Solution.lengthOfLongestSubstring(Solution,"abcabcbb"))
+print(Solution.lengthOfLongestSubstring(Solution,"bbbbb"))
+print(Solution.lengthOfLongestSubstring(Solution,"pwwkew"))
+print(Solution.lengthOfLongestSubstring(Solution," "))
+print(Solution.lengthOfLongestSubstring(Solution,"au"))
