b017f485c1e0967d0003611330d56772405e37e5
Cocakolev <kiril4444@gmail.com>
2022-04-12 12:46:45 -0500

    Started 1260

---
+++ 1260shift2D.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        newGrid = []
+        for x in range(0,(len(grid) * len(grid[0]))):
+            return
+
+
+        return
+
+
+print(Solution.shiftGrid(Solution,[[1,2,3],[4,5,6],[7,8,9]],1))
+
+
