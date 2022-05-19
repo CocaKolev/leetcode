811e5cfce4a986ecbe3ffc322fbb843184ae3b20
CocaKolev <kiril4444@gmail.com>
2022-05-18 23:25:14 -0500

    Formatted with Black to test extension

--- 1twosum.py
+++ 1twosum.py
@@ -1,28 +1,30 @@
 import time
 from typing import List
+
 start_time = time.time()
+
+
 class Solution:
-    #Solve with brute force for memory efficiency 
+    # Solve with brute force for memory efficiency
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for x in range(0,len(nums)):
-            for y in range(x+1, len(nums)):
-                if nums[x]+nums[y] == target:
-                    return x,y
-          
+        for x in range(0, len(nums)):
+            for y in range(x + 1, len(nums)):
+                if nums[x] + nums[y] == target:
+                    return x, y
+
         return
 
-
-    #Solve with Hash for speed efficiency
+    # Solve with Hash for speed efficiency
     def twoSum2(self, nums: List[int], target: int) -> List[int]:
         new_dict = {}
-        for index,value in enumerate(nums):
+        for index, value in enumerate(nums):
             secondNum = target - nums[index]
             if secondNum in new_dict:
                 return index, new_dict[secondNum]
             new_dict[value] = index
         return
 
-        #USES FOR RANGE LOOP
+        # USES FOR RANGE LOOP
         # new_dict = {}
         # for x in range(len(nums)):
         #     secondNum = target - nums[x]
@@ -30,11 +32,11 @@ class Solution:
         #         return x, new_dict[secondNum]
         #     new_dict[nums[x]] = x
 
-        #return
+        # return
 
 
-print(Solution.twoSum2(Solution,[3,2,4],6))
-print(Solution.twoSum2(Solution,[2,7,11,15],9))
-print(Solution.twoSum2(Solution,[3,3],6))
+print(Solution.twoSum2(Solution, [3, 2, 4], 6))
+print(Solution.twoSum2(Solution, [2, 7, 11, 15], 9))
+print(Solution.twoSum2(Solution, [3, 3], 6))
 
-print("Done %s seconds" % (time.time()-start_time))
+print("Done %s seconds" % (time.time() - start_time))
--- leetcode12/nthnode.py
+++ leetcode12/nthnode.py
@@ -1,4 +1,6 @@
 from typing import List, Optional
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -8,33 +10,35 @@ class ListNode:
 def removeNthFromEnd(head: Optional[ListNode], n: int) -> Optional[ListNode]:
     if head.next == None:
         return None
-    mylist= []
+    mylist = []
 
     while head != None:
         mylist.append(head)
-        head=head.next
-    
-    indexToRemove=len(mylist)-n
+        head = head.next
 
-    if indexToRemove==0:
+    indexToRemove = len(mylist) - n
+
+    if indexToRemove == 0:
         return mylist[1]
-    
-    mylist[indexToRemove-1].next=mylist[indexToRemove].next
-    
+
+    mylist[indexToRemove - 1].next = mylist[indexToRemove].next
+
     return mylist[0]
+
 
 def newListNode(nums):
     ln = ListNode(nums[0])
-    ln2=ln
-    for x in range(1,len(nums)):
-        ln2.next=ListNode(nums[x])
-        ln2=ln2.next
+    ln2 = ln
+    for x in range(1, len(nums)):
+        ln2.next = ListNode(nums[x])
+        ln2 = ln2.next
     return ln
+
 
 def printListNode(node: ListNode):
     while node != None:
         print(node.val)
-        node=node.next
+        node = node.next
 
 
-printListNode(removeNthFromEnd(newListNode([2]),1))
+printListNode(removeNthFromEnd(newListNode([2]), 1))
